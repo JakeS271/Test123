6b39d51ca7e2c6db82e22a442b2ce875169f79f4
Jake Spence <jake.spence@ad.aie.edu>
2016-09-12 10:47:34 +0930

    Jake - Testing with Lerp

--- Assets/Scripts/GlideControls.cs
+++ Assets/Scripts/GlideControls.cs
@@ -4,7 +4,7 @@ using InControl;
 
 public class GlideControls : MonoBehaviour {
 
-    public float smooth = 0.75f, tiltAngle = 1.0f;
+    public float smooth = 0.75f, tiltAngle = 1.0f, leftBoarder = 10.0f, rightBoarder= -10.0f;
     private float lift, drag;
     //private Vector3 velocity = new Vector3(0,0,30);
     public Vector3 acceleration, force, maxVelocity;
@@ -25,14 +25,41 @@ public class GlideControls : MonoBehaviour {
         float horizontal = Input.GetAxis("Horizontal") + device.LeftStick.X;
         float vertical = Input.GetAxis("Vertical") + device.LeftStick.Y;
 
-        if (device != null) ;
+        if (device != null) 
         {
-            angles.x = Mathf.Clamp(angles.x + vertical * tiltAngle * Time.deltaTime, -60, 90);
-            angles.z = Mathf.Clamp(angles.z + horizontal * -tiltAngle * Time.deltaTime, -90, 90);
-            angles.y = angles.y + horizontal * tiltAngle * Time.deltaTime;
-            transform.eulerAngles = angles;            
+            if (horizontal != 0)
+            {
+                angles.z = Mathf.Clamp(angles.z + horizontal * -tiltAngle * Time.deltaTime, -90, 90);
+                angles.y = angles.y + horizontal * tiltAngle * Time.deltaTime;
+                transform.eulerAngles = angles;
+            }
+            if (vertical != 0)
+            {
+                angles.x = Mathf.Clamp(angles.x + vertical * tiltAngle * Time.deltaTime, -60, 90);
+                transform.eulerAngles = angles;
+            }
+            else
+            {
+                angles.z = 0;
+                Vector3 q = new Vector3(transform.rotation.x, transform.rotation.y, 0);
+                transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, q, Time.deltaTime);
+            }
+            
 
+            
             transform.position += transform.forward * Time.deltaTime * 30.0f; //* velocity;
+            //if(angles.y <= leftBoarder && angles.y >= rightBoarder)
+            //{
+            //    if (horizontal == 0)
+            //    {
+            //        angles.z = angles.z - tiltAngle * Time.deltaTime;
+            //        print("Is Between");
+            //    }
+            //}
+            
+            
+
         }
+      
 	}
 }
