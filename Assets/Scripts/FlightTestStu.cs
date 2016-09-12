ee5452f4f9b0dfebcbd854b55aa8b25d22bc0bf2
Stuart Burfield <stuartanthonyb@gmail.com>
2016-09-12 12:08:11 +0930

    Added rotating the player back to 0 in test file

--- Assets/Scripts/FlightTestStu.cs
+++ Assets/Scripts/FlightTestStu.cs
@@ -1,29 +1,106 @@
 ﻿using UnityEngine;
 using System.Collections;
+using InControl;
 
 public class FlightTestStu : MonoBehaviour
 {
 
-    public float tiltAngle = 10.0F;
-    public float smooth = 2.0F;
+    public float smooth = 0.75f, tiltAngle = 1.0f, leftBoarder = 10.0f, rightBoarder = -10.0f;
+    private float lift, drag;
+    //private Vector3 velocity = new Vector3(0,0,30);
+	bool moveLeft = false;
+	bool moveRight = false;
+    public Vector3 acceleration, force, maxVelocity;
 
-	// Use this for initialization
-	void Start ()
+    private Vector3 angles = Vector3.zero;
+
+    // Use this for initialization
+    void Start()    {    }
+
+    // Update is called once per frame
+    void FixedUpdate()
     {
-	
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        if (transform.eulerAngles.x >= 0 && transform.eulerAngles.x <= 89.9)
-        {
-            if(Input.GetKey("w") || Input.GetKey("up"))
-            {
-                transform.Rotate(tiltAngle * smooth, 0, 0);
-            }
-            
+        InputDevice device = InputManager.Devices[0];
+
+        float horizontal = Input.GetAxis("Horizontal") + device.LeftStick.X;
+        float vertical = Input.GetAxis("Vertical") + device.LeftStick.Y;
+
+        if (device != null)
+        {			
+//			if(Input.GetKeyDown("left") || Input.GetKeyDown("a"))
+//			{
+//				moveLeft = true;
+//			}
+//			if(Input.GetKeyUp("left") || Input.GetKeyUp("a"))
+//			{
+//				moveLeft = false;
+//			}
+//			if(Input.GetKeyDown("right") || Input.GetKeyDown("d"))
+//			{
+//				moveRight = true;
+//			}	
+//			if(Input.GetKeyUp("right") || Input.GetKeyUp("d"))
+//			{
+//				moveRight = false;
+//			}	
+
+			// mathf.Clamp is to keep the numbers between the range, -90,90
+			// 
+            angles.z = Mathf.Clamp(angles.z + horizontal * -tiltAngle * Time.deltaTime, -90, 90); 
+			angles.z = Mathf.LerpAngle (angles.z, 0, Time.deltaTime * 2.0f);
+
+            angles.y = angles.y + horizontal * tiltAngle * Time.deltaTime;
+            angles.x = Mathf.Clamp(angles.x + vertical * tiltAngle * Time.deltaTime, -60, 90); 
+            transform.eulerAngles = angles;           
+
+            transform.position += transform.forward * Time.deltaTime * 30.0f;
+
+//			if (transform.rotation.z < 0 && moveLeft == false)
+//			{
+//				
+//				//angles.z = 0;
+//				//Vector3 q = new Vector3(transform.rotation.x, transform.rotation.y, 0);
+//				//transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, q, Time.deltaTime);			
+//			}
+//			else if (transform.rotation.z > 0 && moveRight == false)
+//			{
+//				//angles.z = 0;
+//				//Vector3 q = new Vector3(transform.rotation.x, transform.rotation.y, 0);
+//				//transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, q, -Time.deltaTime);	
+//			}
+
+            //if (horizontal != 0)
+            //{
+            //    angles.z = Mathf.Clamp(angles.z + horizontal * -tiltAngle * Time.deltaTime, -90, 90);
+            //    angles.y = angles.y + horizontal * tiltAngle * Time.deltaTime;
+            //    transform.eulerAngles = angles;
+            //}
+            //if (vertical != 0)
+            //{
+            //    angles.x = Mathf.Clamp(angles.x + vertical * tiltAngle * Time.deltaTime, -60, 90);
+            //    transform.eulerAngles = angles;
+            //}
+            //else
+            //{
+            //    angles.z = 0;
+            //    Vector3 q = new Vector3(transform.rotation.x, transform.rotation.y, 0);
+            //    transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, q, Time.deltaTime);
+            //}
+
+
+            //* velocity;
+            //if(angles.y <= leftBoarder && angles.y >= rightBoarder)
+            //{
+            //    if (horizontal == 0)
+            //    {
+            //        angles.z = angles.z - tiltAngle * Time.deltaTime;
+            //        print("Is Between");
+            //    }
+            //}
+
+
+
         }
+
     }
 }
-
