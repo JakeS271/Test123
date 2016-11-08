4fb25ba29ed0633c9348fdae89e0021265b4ace2
Stuart Burfield <stuartanthonyb@gmail.com>
2016-11-09 10:26:00 +1030

    Last commit

--- Assets/Scripts/TestCamera.cs
+++ Assets/Scripts/TestCamera.cs
@@ -10,37 +10,37 @@ public class TestCamera : MonoBehaviour
     public float fallingPositionOffset = 30.0f;
     public Component flightScript;
 
-    private GliderControlsWithForce glider;
-
-    // Use this for initialization
-    void Start()
-    {
-        glider = followTarget.transform.parent.gameObject.GetComponent<GliderControlsWithForce>();
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (followTarget)
-        {
-            if (Vector3.Distance(transform.position, followTarget.transform.position) > 3)
-            {
-                GameObject target = followTarget.transform.parent.gameObject;
-
-                if (glider.acceleration >= 1)
-                {
-                    transform.rotation = Quaternion.Lerp(transform.rotation, followTarget.transform.rotation, rotationOffset);
-                }
-                else if (glider.acceleration <= 0)
-                {
-                    transform.LookAt(target.transform);
-                    transform.position
-                }
-
-                transform.rotation = Quaternion.Lerp(transform.rotation, followTarget.transform.rotation, rotationOffset);
-                transform.position = Vector3.Lerp(transform.position, followTarget.transform.position, positionOffset);
-
-            }
-        }
-    }
+//    private GliderControlsWithForce glider;
+//
+//    // Use this for initialization
+//    void Start()
+//    {
+//        glider = followTarget.transform.parent.gameObject.GetComponent<GliderControlsWithForce>();
+//    }
+//
+//    // Update is called once per frame
+//    void FixedUpdate()
+//    {
+//        if (followTarget)
+//        {
+//            if (Vector3.Distance(transform.position, followTarget.transform.position) > 3)
+//            {
+//                GameObject target = followTarget.transform.parent.gameObject;
+//
+//                if (glider.acceleration >= 1)
+//                {
+//                    transform.rotation = Quaternion.Lerp(transform.rotation, followTarget.transform.rotation, rotationOffset);
+//                }
+//                else if (glider.acceleration <= 0)
+//                {
+//                    transform.LookAt(target.transform);
+//                    transform.position
+//                }
+//
+//                transform.rotation = Quaternion.Lerp(transform.rotation, followTarget.transform.rotation, rotationOffset);
+//                transform.position = Vector3.Lerp(transform.position, followTarget.transform.position, positionOffset);
+//
+//            }
+//        }
+//    }
 }
